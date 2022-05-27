e135a24c52c91cbfb90b49465e960a49947529ad
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-05-27 13:13:16 +0530

    sign in , sign up

--- Innvonix/frontend/src/App.js
+++ Innvonix/frontend/src/App.js
@@ -1,8 +1,12 @@
 import Navbar from "./Components/Navbar";
+import SignIn from "./Components/SignIn";
+import SignUp from "./Components/SignUp";
 
 function App() {
   return <>
     <Navbar />
+    {/* <SignIn /> */}
+    <SignUp />
   </>
 }
 
