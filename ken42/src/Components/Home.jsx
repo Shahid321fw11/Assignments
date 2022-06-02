7069a2219fb2613c2f042aead964a059c33449fe
Shahid321fw11 <shahid_fw11_321@masai.school>
2022-06-02 05:32:50 +0530

    last part remaining

--- ken42/src/Components/Home.jsx
+++ ken42/src/Components/Home.jsx
@@ -6,11 +6,21 @@ import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
+import { TimePickerComponent } from '@syncfusion/ej2-react-calendars';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
 
 
 const Home = () => {
     const day_array = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+
     const [state, setState] = useState({
+        start_time: '',
+        end_time: '',
         day: '',
         professor: '',
         classRoom: '',
@@ -43,6 +53,22 @@ const Home = () => {
                     {day_array.map((day, index) => <MenuItem key={index} value={day}>{day}</MenuItem>)}
                 </Select>
             </FormControl>
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120, marginTop: '1.5rem' }}>
+                <TimePickerComponent placeholder='Select Starting Time'
+                    format="HH:mm" step={30}
+                    value={state.start_time}
+                    onChange={(event) => setState({ ...state, start_time: event.target.value })}
+                >
+                </TimePickerComponent>
+            </FormControl>
+
+            <FormControl variant="standard" sx={{ m: 1, minWidth: 120, marginTop: '1.5rem' }}>
+                <TimePickerComponent placeholder='Select Ending Time' format="HH:mm" step={30} value={state.end_time}>
+                </TimePickerComponent>
+            </FormControl>
+
+
 
             {/* select dropdown  professor list from localstorage */}
             <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
@@ -104,11 +130,48 @@ const Home = () => {
                     }
                 </Select>
             </FormControl>
-            <Button variant="contained" color="primary" type="submit" style={{ marginTop: '1rem' }} onClick={handleSubmit} >
+            <Button variant="contained" color="primary" type="submit" style={{ marginTop: '1.5rem' }} onClick={handleSubmit} >
                 Submit
             </Button>
 
 
+        </Paper>
+
+        {/* show all time table */}
+        <Paper elevation={3} style={{ textAlign: 'center', width: '90%', marginLeft: 'auto', marginRight: 'auto', marginTop: '2rem', padding: '1rem' }}>
+            <Typography variant="h6" >
+                All Time Table
+            </Typography>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Day</TableCell>
+                            <TableCell align="right">Start Time</TableCell>
+                            <TableCell align="right">End Time</TableCell>
+                            <TableCell align="right">Professor</TableCell>
+                            <TableCell align="right">ClassRoom</TableCell>
+                            <TableCell align="right">StudentGroup</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {
+                            localStorage.getItem('timeTable') ? JSON.parse(localStorage.getItem('timeTable')).map((timeTable, index) => {
+                                return <TableRow key={index}>
+                                    <TableCell component="th" scope="row">
+                                        {timeTable.day}
+                                    </TableCell>
+                                    <TableCell align="right">{timeTable.start_time}</TableCell>
+                                    <TableCell align="right">{timeTable.end_time}</TableCell>
+                                    <TableCell align="right">{timeTable.professor}</TableCell>
+                                    <TableCell align="right">{timeTable.classRoom}</TableCell>
+                                    <TableCell align="right">{timeTable.studentGroup}</TableCell>
+                                </TableRow>
+                            }) : <></>
+                        }
+                    </TableBody>
+                </Table>
+            </TableContainer>
         </Paper>
     </>
 }
